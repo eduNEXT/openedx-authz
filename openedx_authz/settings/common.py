f5fdbae65e41da3f60e06b192d466168752a0c8d
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-09-10 18:33:56 -0500

    feat: add custom settings for casbin

--- openedx_authz/settings/common.py
+++ openedx_authz/settings/common.py
@@ -4,7 +4,16 @@ Common settings for openedx_authz plugin.
 
 import os
 
+from redis_watcher import WatcherOptions, new_watcher
+
 from openedx_authz import ROOT_DIRECTORY
+
+
+def callback_function(event):
+    """
+    Callback function for the enforcer.
+    """
+    print("\nCallback function for the enforcer, event: {}".format(event))
 
 
 def plugin_settings(settings):
@@ -18,32 +27,16 @@ def plugin_settings(settings):
         settings: The Django settings object
     """
     # Add external third-party apps to INSTALLED_APPS
-    external_apps = [
-        "casbin_adapter.apps.CasbinAdapterConfig",  # Casbin Adapter
-        "dauthz.apps.DauthzConfig",  # Django Authorization library
-    ]
-    for app in external_apps:
-        if app not in settings.INSTALLED_APPS:
-            settings.INSTALLED_APPS.append(app)
+    casbin_adapter_app = "casbin_adapter.apps.CasbinAdapterConfig"
+    if casbin_adapter_app not in settings.INSTALLED_APPS:
+        settings.INSTALLED_APPS.append(casbin_adapter_app)
 
-    # Add middleware for authorization
-    middleware_class = "dauthz.middlewares.request_middleware.RequestMiddleware"
-    settings.MIDDLEWARE = settings.MIDDLEWARE + [middleware_class]
-
-    # Add authorization configuration
+    # Add Casbin configuration
     settings.CASBIN_MODEL = os.path.join(ROOT_DIRECTORY, "model.conf")
-    settings.DAUTHZ = {
-        "DEFAULT": {
-            "MODEL": {
-                "CONFIG_TYPE": "file",
-                "CONFIG_FILE_PATH": settings.CASBIN_MODEL,
-                "CONFIG_TEXT": "",
-            },
-            "ADAPTER": {
-                "NAME": "casbin_adapter.adapter.Adapter",
-            },
-            "LOG": {
-                "ENABLED": True,
-            },
-        },
-    }
+    watcher_options = WatcherOptions()
+    watcher_options.host = "redis"
+    watcher_options.port = 6379
+    watcher_options.optional_update_callback = callback_function
+    watcher = new_watcher(watcher_options)
+    settings.CASBIN_WATCHER = watcher
+    settings.CASBIN_ADAPTER = "openedx_authz.engine.adapter.ExtendedAdapter"
